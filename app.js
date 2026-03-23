dc9d25bd8660590553c04d3b2dfad939037e4d8d
Keremci01 <keremyilmaz1734@gmail.com>
2026-03-23 16:38:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const chart = new Chart(ctx, {
             data: dataPoints,
             borderWidth: 2,
             showLine: true,
-            tension: 0.3
+            tension: 0.3,
+            pointRadius: 5
         }]
     },
     options: {
@@ -18,12 +19,22 @@ const chart = new Chart(ctx, {
             x: {
                 type: 'linear',
                 position: 'bottom',
+                min: 0,
+                max: 60,
+                ticks: {
+                    stepSize: 10
+                },
                 title: {
                     display: true,
                     text: 'Zaman (s)'
                 }
             },
             y: {
+                min: 0,
+                max: 100,
+                ticks: {
+                    stepSize: 10
+                },
                 title: {
                     display: true,
                     text: 'Sürat (m/s)'
@@ -37,7 +48,7 @@ function addData() {
     const time = parseFloat(document.getElementById('time').value)
     const speed = parseFloat(document.getElementById('speed').value)
 
-    if(isNaN(time) || isNaN(speed)) return
+    if (isNaN(time) || isNaN(speed)) return
 
     dataPoints.push({ x: time, y: speed })
 
